498b7b7a6bddfc6fdca06864aec4210e4850e3f4
Rajat Bansal <rjtbansal@gmail.com>
2020-10-05 22:12:29 -0400

    rendering styled burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from "react";
 import classes from "./Layout.module.scss";
 
-const layout = (props) => (
+const Layout = (props) => (
   <React.Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </React.Fragment>
 );
 
-export default layout;
+export default Layout;
