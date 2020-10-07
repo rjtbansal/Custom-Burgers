e5642e2257b87961529a107bf6f763712ffbd5e3
Rajat Bansal <rjtbansal@gmail.com>
2020-10-06 22:20:27 -0400

    added message if no ingredients present

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,20 +2,24 @@ import React from "react";
 import classes from "./Burger.module.scss";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const Burger = (props) => {
-
   const { ingredients } = props;
   let mappedIngredients = [];
-  for (const ingredient in ingredients) {
-    const quantity = ingredients[ingredient];
-    for (let i = 0; i < quantity; i++) {
-      mappedIngredients.push(<BurgerIngredient key={`${ingredient}${i}`} type={ingredient} />);
-    }
+    for (const ingredient in ingredients) {
+      const quantity = ingredients[ingredient];
+      for (let i = 0; i < quantity; i++) {
+        mappedIngredients.push(
+          <BurgerIngredient key={`${ingredient}${i}`} type={ingredient} />
+        );
+      }
+  }
+  if (mappedIngredients.length === 0) {
+    mappedIngredients = <p>Please start adding ingredients</p>
   }
 
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      { mappedIngredients }
+      {mappedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
