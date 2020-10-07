24e427f8e22ca4ac012e44b82e0d58b065c39601
Rajat Bansal <rjtbansal@gmail.com>
2020-10-06 22:05:07 -0400

    added state to BurgerBuilder to dynamically render Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,20 @@ import React from "react";
 import classes from "./Burger.module.scss";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const Burger = (props) => {
+
+  const { ingredients } = props;
+  let mappedIngredients = [];
+  for (const ingredient in ingredients) {
+    const quantity = ingredients[ingredient];
+    for (let i = 0; i < quantity; i++) {
+      mappedIngredients.push(<BurgerIngredient type={ingredient} />);
+    }
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      { mappedIngredients }
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
