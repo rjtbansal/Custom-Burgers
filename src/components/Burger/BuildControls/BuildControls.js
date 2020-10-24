fdc29c143d105843b8f8cbf57e04ba389c51dfc8
Rajat Bansal <rajat@thesustainability.io>
2020-10-23 22:19:38 -0400

    added build controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from "./BuildControls.module.scss";
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: "Salad" , type: "salad"},
+  { label: "Bacon" , type: "bacon"},
+  { label: "Cheese" , type: "cheese"},
+  { label: "Meat" , type: "meat"},
+];
+const BuildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {
+      controls.map(control => (
+        <BuildControl key={control.type} label={control.label} />
+      ))
+    }
+  </div>
+);
+
+export default BuildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
@@ -14,7 +15,7 @@ class BurgerBuilder extends Component {
     return (
       <React.Fragment>
         <Burger ingredients={this.state.ingredients} />
-        <div> Build Controls</div>
+        <BuildControls />
       </React.Fragment>
     );
   }
