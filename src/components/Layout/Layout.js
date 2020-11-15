76552e389d69b369edbcef5c0cff1255e3966f79
Rajat Bansal <rjtbansal@gmail.com>
2020-11-15 16:31:42 -0500

    side drawer fully functional

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.scss";
 
-const Layout = (props) => (
-  <React.Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </React.Fragment>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+
+  sideDrawerToggleHandler = () => {
+    //never directly mutate the state when it depends on the old state. always toggle from old state
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer }
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,22 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.module.scss";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const SideDrawer = (props) => {
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <React.Fragment>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={`${classes.SideDrawer} ${props.open ? classes.Open : classes.Close}`}>
+        <div className={classes.Logo}>
+          <Logo />
         </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </React.Fragment>
   );
 };
 
