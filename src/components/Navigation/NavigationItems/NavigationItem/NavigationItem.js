4af2ba9e5c2d59c5e29ee25b1d355b854d9f5589
Rajat Bansal <rjtbansal@gmail.com>
2020-12-31 12:29:43 -0500

    implemented navigation links

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./NavigationItem.module.scss";
 
 const NavigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a href={props.link} className={props.active && classes.active}>
+    {/* NavLink adds its own active class at runtime but since we are using CSS modules here our active class defined in CSS wont be recognized since our classnames
+    are hashed to a unique value. To solve this we can used NavLinks activeClassName attribute to specify our own active class and hence apply that CSS */}
+    {/* without exact attribute our path will match for all links and so active class will be applied to all tabs as we click */}
+    <NavLink exact to={props.link} activeClassName={classes.active}>
       {props.children}
-    </a>
+    </NavLink>
   </li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,10 +5,8 @@ import classes from "./NavigationItems.module.scss";
 const NavigationItems = () => (
   <ul className={classes.NavigationItems}>
     {/* for boolean prop we can just pass the name. Below we pass active */}
-    <NavigationItem link="/" active>
-      Burger Builder
-    </NavigationItem>
-    <NavigationItem link="/"> Checkout</NavigationItem>
+    <NavigationItem link="/"> Burger Builder </NavigationItem>
+    <NavigationItem link="/orders">Orders</NavigationItem>
   </ul>
 );
 
