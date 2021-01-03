a1306e3a28ecc58ddf930906b503f1e7bf41c717
Rajat Bansal <rjtbansal@gmail.com>
2021-01-03 15:21:01 -0500

    redux setup. added reducer, action and created store setup

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+import * as actionTypes from './actions';
+
+const initalState = {
+  ingredients: null,
+  totalPrice: 4,
+};
+
+const reducer = (state = initalState, action) => {
+
+  
+};
+
+export default reducer;
