ceb5cb2a4cc6304c2b3b78946fa5de628950f915
Rajat Bansal <rjtbansal@gmail.com>
2021-01-08 14:59:22 -0500

    sign up working

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,7 @@
 import * as actionTypes from './actionTypes';
+import axios from "axios";
 
+const api_key = process.env.REACT_APP_FIREBASE_API_KEY;
 export const authStart = () => {
   return {
     type: actionTypes.AUTH_START
@@ -22,7 +24,21 @@ export const authFail = (error) => {
 
 /**will be an async call to authentication request */
 export const auth = (email, password) => {
-  return dispatch => {
+  return async(dispatch) => {
     dispatch(authStart());
-  }
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${api_key}`,
+        authData
+      );
+      dispatch(authSuccess(response.data));
+    } catch (error) {
+      dispatch(authFail(error));
+    }
+    }
 }
