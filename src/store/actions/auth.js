dcc5ff9f9607e289e6bbb6138da1aa059914055d
Rajat Bansal <rjtbansal@gmail.com>
2021-01-08 19:35:25 -0500

    storing token in redux

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData,
+    idToken: token,
+    userId
   };
 };
 
@@ -37,7 +38,7 @@ export const auth = (email, password, isSignup) => {
     }
     try {
       const response = await axios.post(url, authData);
-      dispatch(authSuccess(response.data));
+      dispatch(authSuccess(response.data.idToken, response.data.localId));
     } catch (error) {
       dispatch(authFail(error));
     }
