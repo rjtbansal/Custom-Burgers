5390e6e6e0852b5a4e92fd371743ced1e5060297
Rajat Bansal <rjtbansal@gmail.com>
2021-01-10 08:05:04 -0500

    storing data in persistent state and auto logout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Route, Switch, withRouter } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
+import * as actions from "./store/actions/";
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.onTryAutoSignup();
+  }, [props]);
   return (
     <div>
       <Layout>
@@ -23,4 +28,12 @@ function App() {
   );
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
+
+//wrapping entire App component breaks the router. So we need to wrap it with withRouter HOC provided by react-router-dom
+//this will allow props to get passed to App component
+export default withRouter(connect(null, mapDispatchToProps)(App));
