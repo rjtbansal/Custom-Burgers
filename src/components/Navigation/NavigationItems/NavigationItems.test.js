0602132ad993bbc0f6412d5fe88a603931810baa
Rajat Bansal <rjtbansal@gmail.com>
2021-01-18 21:42:05 -0500

    added 3rd test to NavigationItems

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,7 +1,7 @@
 /**enzyme package allows us to test components in isolation instead of requiring entire React App to run */
 import React from "react";
 import { configure, shallow } from "enzyme";
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from "enzyme-adapter-react-16";
 import NavigationItems from "./NavigationItems";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
@@ -21,7 +21,14 @@ describe("<NavigationItems />", () => {
 
   it("should render three <NavigationItem /> components if authenticated", () => {
     // passing isAuthenticated prop will pass it as true
-    wrapper.setProps({ isAuthenticated: true}); //setProps allows us to pass props in helper function
+    wrapper.setProps({ isAuthenticated: true }); //setProps allows us to pass props in helper function
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+  it("should render /logout only if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    // passing isAuthenticated prop will pass it as true
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
   });
 });
